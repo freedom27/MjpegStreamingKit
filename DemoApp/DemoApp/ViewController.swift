189910dec67d06d13d11188501ad6490c29e4a4b
freedom <stefano.vettor@mac.com>
2016-03-28 20:42:44 +0200

    added demo app

---
+++ DemoApp/DemoApp/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Stefano Vettor on 28/03/16.
+//  Copyright © 2016 Stefano Vettor. All rights reserved.
+//
+
+import UIKit
+import MjpegStreamingKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var url: NSURL?
+    
+    var playing = false
+    
+    var streamingController: MjpegStreamingController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        streamingController = MjpegStreamingController(imageView: imageView)
+        streamingController.didStartLoading = { [unowned self] in
+            self.loadingIndicator.startAnimating()
+        }
+        streamingController.didFinishLoading = { [unowned self] in
+            self.loadingIndicator.stopAnimating()
+        }
+        
+        url = NSURL(string: "http://webcams.hotelcozumel.com.mx:6003/axis-cgi/mjpg/video.cgi?resolution=320x240&dummy=1458771208837")
+        streamingController.contentURL = url
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func playAndStop(sender: AnyObject) {
+        if playing {
+            playButton.setTitle("Play", forState: .Normal)
+            streamingController.stop()
+            playing = false
+        } else {
+            
+            streamingController.play()
+            playing = true
+            playButton.setTitle("Stop", forState: .Normal)
+        }
+    }
+    
+    
+}
+
+
